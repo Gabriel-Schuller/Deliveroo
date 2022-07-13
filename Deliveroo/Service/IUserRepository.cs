d3fdbba859f269518f05a0ab9c85dab89cfc1823
Schullert Gabriel <schullergabriel13@gmail.com>
2022-07-13 19:25:00 +0300

    Implement UserRepository
    User Repository has been implemented with functions to get user by id,  get all users, get all administrators, and get user by email address.

--- Deliveroo/Service/IUserRepository.cs
+++ Deliveroo/Service/IUserRepository.cs
@@ -7,6 +7,13 @@ namespace Deliveroo.Service
 {
     public interface IUserRepository
     {
+        Task<User> GetById(Guid id);
+
+
+        Task<List<User>> GetAllUsersAsync();
+        Task<List<User>> GetAllAdministratorsAsync();
+
+        Task<User> GetUserByEmail(string email);
 
     }
 }
---
+++ Deliveroo/Service/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Deliveroo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deliveroo.Service.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAllAdministratorsAsync()
+        {
+            return await _context.Users.Where(u => u.Role == "Administrator").ToListAsync();
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetById(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.EmailAddress == email);
+        }
+
+    }
+}
