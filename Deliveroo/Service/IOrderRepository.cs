fc41fe5f62e9d0810df5731bd01f667429a2ff03
Schullert Gabriel <schullergabriel13@gmail.com>
2022-07-13 20:07:45 +0300

    Added Order Repository
    Repository for order has functions to get all orders, get specific order by id, all orders and orders of a specific user

---
+++ Deliveroo/Service/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Deliveroo.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Deliveroo.Service
+{
+    public interface IOrderRepository
+    {
+        //Get Orders by date to be implemented
+        Task<List<Order>> GetAllOrders();
+        
+        Task<Order> GetOrderById(Guid orderId);
+        Task<List<Order>> GetUserOrders(Guid id);
+    }
+}
---
+++ Deliveroo/Service/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Deliveroo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deliveroo.Service.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DataContext _context;
+
+        public OrderRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> GetOrderById(Guid orderId)
+        {
+            return await _context.Orders.FindAsync(orderId);
+        }
+
+        public async Task<List<Order>> GetUserOrders(Guid id)
+        {
+           
+
+            return await _context.Orders.Where(o => o.UserID == id).ToListAsync();
+        }
+    }
+}
