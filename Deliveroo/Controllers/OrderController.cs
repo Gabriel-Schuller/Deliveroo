df78eaf4d272ec8e8a2ea63af333966022f68fb8
Schullert Gabriel <schullergabriel13@gmail.com>
2022-07-16 11:20:09 +0300

    Add basic Order Controller to implement repository methods

--- Deliveroo/Controllers/OrderController.cs
+++ Deliveroo/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using Deliveroo.Data.Entities;
 using Deliveroo.Models;
 using Deliveroo.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,30 +15,76 @@ namespace Deliveroo.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IAddressRepository _repository;
         private readonly LinkGenerator _linkGenerator;
         private readonly IMapper _mapper;
         private readonly IBaseRepository _baseRepository;
 
-        public OrderController(, LinkGenerator linkGenerator, IMapper mapper, IBaseRepository baseRepository)
+        public OrderController(IAddressRepository repository, LinkGenerator linkGenerator, IMapper mapper, IBaseRepository baseRepository)
         {
+            _repository = repository;
             _linkGenerator = linkGenerator;
             _mapper = mapper;
             _baseRepository = baseRepository;
         }
 
-        //[HttpGet]
-        //public async Task<List<AddressModel>> Get()
-        //{
-        //    try
-        //    {
-        //        return await 
-        //    }
-        //    catch (System.Exception)
-        //    {
+        [HttpGet]
+        public async Task<ActionResult<List<Address>>> Get()
+        {
+            try
+            {
+                return await _repository.GetAllAddresses();
 
-        //        throw;
-        //    }
-        //}
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Server error");
+            }
+        }
+
+        [HttpGet("{userID}")]
+        public async Task<ActionResult<List<Address>>> GetAddressByUser(Guid userID)
+        {
+            try
+            {
+                return await _repository.GetAllUserAddresses(userID);
+
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Server error");
+            }
+        }
+
+        [HttpGet("/city/{city}")]
+        public async Task<ActionResult<List<Address>>> GetAddressByCity(string city)
+        {
+            try
+            {
+                return await _repository.GetAddressesByCity(city);
+
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Server error");
+            }
+        }
+
+        [HttpGet("/postalcode/{code}")]
+        public async Task<ActionResult<List<Address>>> GetAddressByCode(string code)
+        {
+            try
+            {
+                return await _repository.GetAddressesByPostalCode(code);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Server error");
+            }
+        }
 
     }
 }
---
+++ Deliveroo/Migrations/20220716081923_UserAddsPersonalAddress.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Deliveroo.Migrations
+{
+    public partial class UserAddsPersonalAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressID",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AddressID",
+                table: "Users",
+                column: "AddressID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Address_AddressID",
+                table: "Users",
+                column: "AddressID",
+                principalTable: "Address",
+                principalColumn: "AddressID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Address_AddressID",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AddressID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AddressID",
+                table: "Users");
+        }
+    }
+}
